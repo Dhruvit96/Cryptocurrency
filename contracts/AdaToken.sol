67b80f9f77cfaf4a2824f17347a0dd49db0a19dc
Dhruvit Maniya <maniyadhruvit@gmail.com>
2021-07-29 16:04:12 +0530

    Added name and starndard

--- contracts/AdaToken.sol
+++ contracts/AdaToken.sol
@@ -1,6 +1,10 @@
 pragma solidity ^0.5.2;
 
 contract AdaToken {
+    
+	string public name = "ADA Token";
+	string public symbol = "ADA";
+	string public standard = "ADA Token v1.0";
 
 	uint256 public totalSupply;
 
--- test/AdaToken.js
+++ test/AdaToken.js
@@ -1,6 +1,22 @@
 const AdaToken = artifacts.require("./AdaToken");
 
 contract('AdaToken', (accounts) => {
+
+    it('initializes the contract with correct value', function () {
+        return AdaToken.deployed().then(function (instance) {
+            tokenInstance = instance;
+            return tokenInstance.name();
+        }).then(function (name) {
+            assert.equal(name, 'ADA Token', 'has the correct name');
+            return tokenInstance.symbol();
+        }).then(function (symbol) {
+            assert.equal(symbol, 'ADA', 'has the correct symbol');
+            return tokenInstance.standard();
+        }).then(function (standard) {
+            assert.equal(standard, 'ADA Token v1.0', 'has the correct standard');
+        })
+    })
+
     it('set Total supply', () => {
         return AdaToken.deployed().then((instance) => {
             tokenInstance = instance;
