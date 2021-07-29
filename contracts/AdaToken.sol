08c4883c1d0b1a163618b2fbbf377d20f4fe5ee5
Dhruvit Maniya <maniyadhruvit@gmail.com>
2021-07-29 17:01:37 +0530

    added transfer event and method

--- contracts/AdaToken.sol
+++ contracts/AdaToken.sol
@@ -1,18 +1,31 @@
 pragma solidity ^0.5.2;
 
 contract AdaToken {
-    
-	string public name = "ADA Token";
-	string public symbol = "ADA";
-	string public standard = "ADA Token v1.0";
+    string public name = "ADA Token";
+    string public symbol = "ADA";
+    string public standard = "ADA Token v1.0";
+    uint256 public totalSupply;
 
-	uint256 public totalSupply;
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
 
     mapping(address => uint256) public balanceOf;
 
-	constructor(uint256 _initialSupply) public {
-        balanceOf[msg.sender] = _initialSupply;
+    constructor(uint256 _initialSupply) public {
+        balanceOf[msg.sender] = _initialSupply; // Key value
         totalSupply = _initialSupply;
     }
 
+    function transfer(address _to, uint256 _value)
+        public
+        returns (bool success)
+    {
+        // check  balance
+        require(balanceOf[msg.sender] >= _value);
+        // Transfer the balance
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        emit Transfer(msg.sender, _to, _value);
+        // return boolean
+        return true;
+    }
 }
--- test/AdaToken.js
+++ test/AdaToken.js
@@ -1,7 +1,7 @@
 const AdaToken = artifacts.require("./AdaToken");
 
 contract('AdaToken', (accounts) => {
-
+    let tokenInstance;
     it('initializes the contract with correct value', function () {
         return AdaToken.deployed().then(function (instance) {
             tokenInstance = instance;
@@ -17,7 +17,7 @@ contract('AdaToken', (accounts) => {
         })
     })
 
-    it('set Total supply', () => {
+    it('allocates the initial supply upon development', () => {
         return AdaToken.deployed().then((instance) => {
             tokenInstance = instance;
             return tokenInstance.totalSupply();
@@ -26,6 +26,32 @@ contract('AdaToken', (accounts) => {
             return tokenInstance.balanceOf(accounts[0]);
         }).then((adminBalance) => {
             assert.equal(adminBalance.toNumber(), 1000000, 'allocates the initial supply to admin account.')
+        })
+    })
+
+    it('transfers token owenership', () => {
+        return AdaToken.deployed().then((instance) => {
+            tokenInstance = instance;
+            return tokenInstance.transfer.call(accounts[1], 777777777777);
+        }).then(assert.fail).catch((error) => {
+            assert(error.message.indexOf('revert') >= 0, 'error message must contain revert.')
+            return tokenInstance.transfer.call(accounts[1], 250000, { from: accounts[0] });
+        }).then((success) => {
+            assert.equal(success, true, 'true');
+            return tokenInstance.transfer(accounts[1], 250000, { from: accounts[0] });
+        }).then((receipt) => {
+            assert.equal(receipt.logs.length, 1, 'triggers one events')
+            assert.equal(receipt.logs[0].event, 'Transfer', '"Transfer event"')
+            assert.equal(receipt.logs[0].args._from, accounts[0], "account 0")
+            assert.equal(receipt.logs[0].args._to, accounts[1], "account 1")
+            assert.equal(receipt.logs[0].args._value, 250000, "value")
+
+            return tokenInstance.balanceOf(accounts[1]);
+        }).then((balance) => {
+            assert.equal(balance.toNumber(), 250000, 'add the amount to the receiving account')
+            return tokenInstance.balanceOf(accounts[0]);
+        }).then((balance) => {
+            assert.equal(balance.toNumber(), 750000, 'deducts the amount from the sending account')
         })
     })
 })
